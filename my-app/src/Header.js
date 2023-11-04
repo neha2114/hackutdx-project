581e71423586f8cc36a3ac79e81b743c73bc7cfc
neha2114 <neha.sapre2114@gmail.com>
2023-11-04 18:50:53 -0500

    has a nice background now

---
+++ my-app/src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+
+  return (
+      <div className="Header">
+          <h1>Find your way home</h1>
+      </div>
+  );
+}
+
+export default Header;
---
+++ my-app/src/Main.js
@@ -0,0 +1,18 @@
+import './Main.css';
+
+function Main() {
+
+  return (
+      <div className="Main">
+        <div className="title">
+            <span>Want a recommendation in less than 5 minutes?</span>
+        </div>
+      
+        <div>
+            <span>Use our calculator: </span>
+        </div> 
+      </div>
+  );
+}
+
+export default Main;

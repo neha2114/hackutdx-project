88f3ff1eee3ac473cc25877f58b2f289b86d39c8
neha2114 <neha.sapre2114@gmail.com>
2023-11-04 13:17:30 -0500

    updated App.js text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I do not want to learn React
         </a>
       </header>
     </div>

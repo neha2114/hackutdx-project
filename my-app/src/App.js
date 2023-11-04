7edddcda5bfae999d9dc6fc589d40175c21f458e
nshibu62 <91027597+nshibu62@users.noreply.github.com>
2023-11-04 14:10:48 -0500

    add a small change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React is really lame
+          Learning React is really lame!
         </a>
       </header>
     </div>

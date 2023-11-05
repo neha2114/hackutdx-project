87828bbe250dbddb71d6cdbaa6a9c79833738cb0
ElenaP268 <paulelena04@gmail.com>
2023-11-05 02:30:15 -0600

    added form to home screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+import Form from './Form'
 import Footer from './Footer';
 function App() {
   return (
     <div className="App">
      <Header />
      <Main />
+     <Form />
      <Footer />
     </div>
   );

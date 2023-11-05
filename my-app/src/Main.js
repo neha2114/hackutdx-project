ad3695646772459a737580461a05b00102d95149
neha2114 <neha.sapre2114@gmail.com>
2023-11-04 19:26:34 -0500

    simple aesthetic ui

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -5,12 +5,8 @@ function Main() {
   return (
       <div className="Main">
         <div className="title">
-            <span>Want a recommendation in less than 5 minutes?</span>
+            <span>Want a recommendation in less than 5 minutes? Use our calculator:</span>
         </div>
-      
-        <div>
-            <span>Use our calculator: </span>
-        </div> 
       </div>
   );
 }
